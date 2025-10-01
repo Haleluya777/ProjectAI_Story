fd8598242d2546675fb51beb28f6246f9e652ea7
Honolulu <88095893+Haleluya777@users.noreply.github.com>
2025-10-01 22:55:03 +0900

    1001

--- Assets/Resources/Scripts/Character/CharacterMap.cs
+++ Assets/Resources/Scripts/Character/CharacterMap.cs
@@ -14,7 +14,8 @@ public class CharacterData
                                    //이 값의 2진수가 00001101101이라면, 1,3,4,6,7번 째 대화 스크립트를 진행했다는 뜻이며, 현재 위치는 7번째 스크립트라는 뜻이다.
 
     public int dialogueLineNum; //현재 대화 진행도 내의 대사 진행도
-    public List<TextAsset> dialogueFiles; //로비에서 캐릭터를 선택했을 때 하는 대화 파일들.
+    public List<TextAsset> dialogueFiles; //로비에서 캐릭터를 선택했을 때 하는 대화 스크립트들의 목록.
+    public TextAsset NextDialogueScript; //다음에 진행할 대화 스크립트.
     public string CharacterFloor; //캐릭터가 있는 층 이름.
 }
 
